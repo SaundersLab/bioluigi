614245252b811f03b0a7b88f83dde7f330849d45
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2017-06-13 16:25:22 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bioluigi",
+    version='0.0.1',
+    author="Daniel Bunintg",
+    author_email="daniel.bunting@jic.ac.uk",
+    url="https://github.com/SaundersLab/bioluigi/",
+    packages=find_packages(),
+
+)
