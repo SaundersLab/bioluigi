627bd3ece0444bc698f2d3ba422b7e33e4a13211
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2017-09-06 12:18:08 +0100

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ setup(
     author_email="daniel.bunting@jic.ac.uk",
     url="https://github.com/SaundersLab/bioluigi/",
     packages=find_packages(),
-
+    install_requires=[
+        'luigi', 'sqlalchemy', 'dill', 'multiprocessing_on_dill'
+    ],
+    dependency_links=[
+        'git+ssh://git@github.com/dnlbunting/luigi.git#egg=luigi',
+    ]
 )
