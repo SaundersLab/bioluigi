34a052055fc9c91469832c9ac1833e2661f60ca1
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2018-08-13 12:51:46 +0100

    Have HISAT task sort bam output and optionally and read group tags

--- bioluigi/tasks/hisat.py
+++ bioluigi/tasks/hisat.py
@@ -40,9 +40,10 @@ class HISAT(CheckTargetNonEmpty, SlurmExecutableTask):
         super().__init__(*args, **kwargs)
         # Set the SLURM request params for this task
         self.mem = 3000
-        self.n_cpu = 4
+        self.n_cpu = 7
         self.partition = "nbi-medium,RG-Diane-Saunders"
         self.dta_cufflinks = False
+        self.rg = None
 
     def output(self):
         output = get_ext(self.input['reads'][0])
@@ -53,9 +54,18 @@ class HISAT(CheckTargetNonEmpty, SlurmExecutableTask):
         }
 
     def work_script(self):
+        rg_commands = ''
+        if self.rg:
+            for k, v in self.rg.items():
+                if k == 'ID':
+                    rg_commands += '--rg-id {0} \\\n'.format(v)
+                else:
+                    rg_commands += '--rg {0}:{1} \\\n'.format(k, v)
+
         return '''#!/bin/bash
                   source HISAT-2.1.0;
                   source samtools-1.7;
+                  rm {bam}.samsort*
                   set -euo pipefail
 
                   hisat2 -t -p {n_cpu} \
@@ -63,7 +73,10 @@ class HISAT(CheckTargetNonEmpty, SlurmExecutableTask):
                          -1 {R1} -2 {R2}  \
                          --new-summary \
                          {dta_cufflinks} \
-                         --summary-file {log}.temp | samtools view -bS - > {bam}.temp
+                         {rg_commands}  \
+                         --summary-file {log}.temp |
+                         samtools view -bS - |
+                         samtools sort -m 2G - --threads 2 -T {bam}.samsort > {bam}.temp
 
                   mv {log}.temp {log}
                   dt=$(date '+%d/%m/%Y %H:%M:%S');
@@ -74,6 +87,7 @@ class HISAT(CheckTargetNonEmpty, SlurmExecutableTask):
                              bam=self.output()['bam'].path,
                              log=self.output()['log'].path,
                              hisat_genome=self.input()['genome'].path,
-                             n_cpu=self.n_cpu-1,
+                             n_cpu=self.n_cpu-3,
+                             rg_commands=rg_commands,
                              R1=self.input()['reads'][0].path,
                              R2=self.input()['reads'][1].path,)
